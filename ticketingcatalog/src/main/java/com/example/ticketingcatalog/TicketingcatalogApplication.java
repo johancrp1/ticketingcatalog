6dd3582349184ddc14a1d075e57c697e8fef7024
johancrp1 <yoquiarango@gmail.com>
2025-11-11 06:40:00 -0500

    Initial commit

---
+++ ticketingcatalog/src/main/java/com/example/ticketingcatalog/TicketingcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketingcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingcatalogApplication.class, args);
+	}
+
+}
---
+++ ticketingcatalog/src/test/java/com/example/ticketingcatalog/TicketingcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketingcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
