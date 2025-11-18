56f4501f9ae9662a6a601694eab08a7875919263
johancrp1 <yoquiarango@gmail.com>
2025-11-18 07:50:08 -0500

    HU1 Completed

--- ticketingcatalog/src/main/java/com/example/ticketingcatalog/controller/EventController.java
+++ ticketingcatalog/src/main/java/com/example/ticketingcatalog/controller/EventController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController // Indica que la clase es un controlador REST
-@RequestMapping("/events") // Base path para todos los endpoints de eventossss
+@RequestMapping("/events") // Base path para todos los endpoints de eventos
 public class EventController {
 
     private final EventServiceImpl eventService;
